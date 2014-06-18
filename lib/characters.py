9bf1ec295ec6c3f4aa0f2b0e3581e805c4c093d0
mikomikositeageru <mgame.owner@gmail.com>
2014-06-18 18:44:45 +0900

    character 'json' save file add and modify setter

--- lib/characters.py
+++ lib/characters.py
@@ -7,36 +7,13 @@ def printu(text):
 	sys.stdout.buffer.write((text+"\n").encode('utf-8'))
 
 class Character:
+	attr = None
 	def __init__(self, path = "../settings/characters.json"):
 		self._path = path
 		self.lib = d40lib.StdIOFile(path)
 		self.jsonData = self.lib.jsonReturn()
 		self.name = {}
 		self.rtName = []
-		# self.values = []
-		# self.status = []
-		# self.charclass = []
-		# self.level = []
-		# self.hp = []
-		# self.ac = []
-		# self.strength = []
-		# self.intelligent = []
-		# self.wisdom = []
-		# self.agillity = []
-		# self.health = []
-		# self.charisma = []
-		# self.res_poison = []
-		# self.res_death = []
-		# self.res_magicstic = []
-		# self.res_parrelize = []
-		# self.res_sturn = []
-		# self.res_breath = []
-		# self.res_magicspell = []
-		# self.res_magicstaff = []
-		# self.skills = []
-		# self.actions = []
-		# self.skillNames = []
-		# self.actionNames = []
 		self.getAllObject()
 		self.ToJson()
 
@@ -44,35 +21,7 @@ class Character:
 		for i in self.jsonData:
 			tmpname = i['charname']
 			self.name[tmpname] = i
-			# self.charname(tmpname) = i
-			# self.values.append(i['values'])
-			# self.status.append(i['status'])
-			# self.charclass.append(i['class'])
-			# self.level.append(i['level'])
-			# self.hp.append(i['hp'])
-			# self.ac.append(i['ac'])
-			# self.strength.append(i['strength'])
-			# self.intelligent.append(i['intelligent'])
-			# self.wisdom.append(i['wisdom'])
-			# self.agillity.append(i['agillity'])
-			# self.health.append(i['health'])
-			# self.charisma.append(i['charisma'])
-			# self.res_poison.append(i['res_poison'])
-			# self.res_death.append(i['res_death'])
-			# self.res_magicstic.append(i['res_magicstic'])
-			# self.res_parrelize.append(i['res_parrelize'])
-			# self.res_sturn.append(i['res_sturn'])
-			# self.res_breath.append(i['res_breath'])
-			# self.res_magicspell.append(i['res_magicspell'])
-			# self.res_magicstaff.append(i['res_magicstaff'])
-			# self.skills.append(i['skills'])
-			# self.actions.append(i['actions'])
-			# self.skillNames.append(i[])
-			# self.actionNames.append(i[])
-
-	def ToJson(self):
-		return self.jsonData
-
+			
 	def getNameis(self, name = None):
 		try:
 			for key in self.name.keys():
@@ -82,17 +31,33 @@ class Character:
 			return None
 
 	def getObjtoName(self, obj, attribute = ""):
-		try:
+		if attribute in obj:
+			print(obj)
+			self.attr = obj[attribute]
+		else:
 			for key in obj.keys():
-				if(key == attribute):
-					return obj[key]
-				elif(type(obj[key]) is dict):
-					return attribute, obj[key][attribute]
-					pass
-				elif(type(obj[key]) is list):
-					self.getObjtoName(obj[key], attribute)
-		except:
-			return None
+				if((type(obj[key]) is dict)):
+					if attribute in obj[key]:
+						self.attr = self.getObjtoName(obj[key], attribute)
+				if(type(obj[key]) is list):
+					for i in obj[key]:
+						self.attr = self.getObjtoName(i, attribute)
+
+		return self.attr
+
+		# retVal = None
+		# 	# print(obj)
+		# for key in obj.keys():
+		# 	# print(key)
+		# 	# print(key+ ":"+ attribute + "\t" +str(type(obj[key])))
+		# 	if(key == attribute):
+		# 		retVal = obj[key]
+		# 	elif(type(obj[key]) is dict):
+		# 		retVal = self.getObjtoName(obj[key], attribute)
+		# 	elif(type(obj[key]) is list):
+		# 		for i in obj[key]:
+		# 			retVal = self.getObjtoName(i, attribute)
+		# return retVal
 
 	def setObjtoName(self, obj, attribute = "", val1 = 0):
 		self.innerSetObj(obj, attribute, val1)
@@ -102,7 +67,6 @@ class Character:
 	def innerSetObj(self, obj, attribute = "", val1 = 0):
 		try:
 			for key in obj.keys():
-				print(key)
 				if(key == attribute):
 					obj[key] = val1
 				elif((type(obj[key]) is dict) or (type(obj[key]) is list)):
@@ -119,70 +83,16 @@ class Character:
 	def printAllObject(self):
 		for key in self.name.keys():
 			print(key, self.name[key])
-		# print("values	:", self.values)
-		# print("status	:", self.status)
-		# print("class	:", self.charclass)
-		# print("", self.level)
-		# print("", self.hp)
-		# print("", self.ac)
-		# print("", self.strength)
-		# print("", self.intelligent)
-		# print("", self.wisdom)
-		# print("", self.agillity)
-		# print("", self.health)
-		# print("", self.charisma)
-		# print("", self.res_poison)
-		# print("", self.res_death)
-		# print("", self.res_magicstic)
-		# print("", self.res_parrelize)
-		# print("", self.res_sturn)
-		# print("", self.res_breath)
-		# print("", self.res_magicspell)
-		# print("", self.res_magicstaff)
-		# print("skills	:", self.skills)
-		# print("ations	:", self.actions)
-		# print("", self.skillNames)
-		# print("", self.actionNames)
 
-
+	def saveFile(self, path = "", name = "", string = ""):
+		self.lib.saveFile(path, name, string)
+		
 if __name__ == "__main__":
 	a = Character("../settings/characters.json")
-	a.printAllObject()
 	# print(a.getNameis("horo"))
 	# print(a.getObjtoName("horo", "status"))
 	a.setObjtoName(a.getNameis("horo"), "hp", 100)
+	print(a.getNameis("horo"))
 	print(a.getObjtoName(a.getNameis("horo"), "hp"))
-	print(a.rtName)
+	a.saveFile(a._path, "", a.lib.Trans(a.rtName))
 
-	# for key in a.charnames["horo"].keys():
-	# 	# print(a.charnames["horo"][key])
-	# 	if(type(a.charnames["horo"][key]) == dict or type(a.charnames["horo"][key]) == list):
-	# 		for val in a.charnames["horo"][key]:
-	# 			print(val)
-
-
-# self.charname = self.jsonData['charname']
-# self.values = self.jsonData['values']
-# self.status = self.jsonData['status']
-# self.charclass = self.status['class']
-# self.level = self.status['level']
-# self.hp = self.status['hp']
-# self.ac = self.status['ac']
-# self.strength = self.status['strength']
-# self.intelligent = self.status['intelligent']
-# self.wisdom = self.status['wisdom']
-# self.agillity = self.status['agillity']
-# self.health = self.status['health']
-# self.charisma = self.status['charisma']
-# self.res_poison = self.status['res_poison']
-# self.res_death = self.status['res_death']
-# self.res_magicstic = self.status['res_magicstic']
-# self.res_parrelize = self.status['res_parrelize']
-# self.res_sturn = self.status['res_sturn']
-# self.res_breath = self.status['res_breath']
-# self.res_magicspell = self.status['res_magicspell']
-# self.res_magicstaff = self.status['res_magicstaff']
-# self.skills = self.jsonData['skills']
-# self.actions = self.jsonData['actions']
-# self.skillNames = []
-# self.actionNames = []
