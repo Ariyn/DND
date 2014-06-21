6f2ec8df6eadeed78cc2ef3c1e3bfa39deb167db
mikomikositeageru <mgame.owner@gmail.com>
2014-06-21 22:52:03 +0900

    다시 커밋

--- lib/librarys.py
+++ lib/librarys.py
@@ -24,11 +24,8 @@ class librarys:
 		self.CharData = None
 		self.ItemData = None
 		self.TextData = None
-<<<<<<< HEAD
 		self.SkillData = None
-=======
 		self.echo = echo.echo()
->>>>>>> c99c159697bda8e032be3cdf8e6e3d94a972fdc9
 		pass
 
 	def roomSetting(self, path = "../settings/roominfo.json", mode = "auto"):
@@ -77,19 +74,18 @@ class librarys:
 			pass
 
 	# def textSetting(self, path = "../settings/texts.json", mode = "auto"):
-		self.TextData = texts.Texts(path)
-		self.textJsonData = ""
-		if(mode == "auto"):
-			self.itemJsonData = self.ItemData.ToJson()
-		else:
-			pass
+	# 	self.TextData = texts.Texts(path)
+	# 	self.textJsonData = ""
+	# 	if(mode == "auto"):
+	# 		self.itemJsonData = self.ItemData.ToJson()
+	# 	else:
+	# 		pass
 
-<<<<<<< HEAD
 	def skillSetting(self, path = "../settings/skills.json", mode = "auto"):
 		self.SkillData = skill.Skills(path)
 		# self.roomfile = RoomData.lib
 		self.SkillData = ""
-=======
+
 class TwitterForm:
 	def __init__(self, path = "../settings/twitterData.json"):
 		self._path = path
@@ -134,8 +130,6 @@ class TwitterForm:
 	def Send(self, userinfo):
 		self.TMData.sendMessages(userinfo)
 				
-
->>>>>>> c99c159697bda8e032be3cdf8e6e3d94a972fdc9
 
 if __name__ == "__main__":
 	
--- lib/test.py
+++ lib/test.py
@@ -3,14 +3,20 @@ import codecs,sys
 import json
 import time
 import threading
+import librarys
 
 def printu(text):
 	sys.stdout.buffer.write((text+"\n").encode('utf-8'))
 
 if __name__ == "__main__":
-	a = []
-	b = {"":""}
-	for i in range(2):
-		a.append(b)
-	print(a)
-	# a = DND_twitter()
+	lib = librarys.librarys()
+	twit = librarys.TwitterForm()
+	# init
+	lib.mobSetting()
+	# get monster
+	monsters = lib.MobData
+	# monsters.printAllObject()
+	monsters.setObjtoName(monsters.getNameis("a"), "charname", "goblin")
+	# monsters.setBasicChar("Goblin", "Goblin", "monster", "monster")
+	# monsters.saveFile(monsters._path,"")
+	monsters.printAllObject()
